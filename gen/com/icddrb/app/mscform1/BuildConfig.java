ffa0429127d0a9be2fa36fb88776242e40f9f455
aumiaik7 <aumi.aik7@gmail.com>
2015-03-25 09:42:59 +0600

    first commit

---
+++ gen/com/icddrb/app/mscform1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.icddrb.app.mscform1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/icddrb/app/mscform1/questions/FormListener.java
@@ -0,0 +1,5 @@
+package com.icddrb.app.mscform1.questions;
+
+public interface FormListener {
+	void gotoForm(String formname);
+}
