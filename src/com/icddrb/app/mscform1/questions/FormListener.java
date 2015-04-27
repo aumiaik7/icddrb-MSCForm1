acb560968c20855733dd719356c581568f80a9eb
aumiaik7 <aumi.aik7@gmail.com>
2015-04-27 11:17:09 +0600

    New name

--- src/com/icddrb/app/mscform1/questions/FormListener.java
+++
@@ -1,5 +0,0 @@
-package com.icddrb.app.mscform1.questions;
-
-public interface FormListener {
-	void gotoForm(String formname);
-}
---
+++ src/com/icddrb/app/salimetricssaliva/questions/FormListener.java
@@ -0,0 +1,5 @@
+package com.icddrb.app.salimetricssaliva.questions;
+
+public interface FormListener {
+	void gotoForm(String formname);
+}
